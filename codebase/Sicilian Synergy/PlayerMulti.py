c1e64b33f1e4e44fa5f8174edef58982cf68aa23
Saksham Rathi <22b1003@iitb.ac.in>
2025-03-22 21:28:07 +0530

    init

--- codebase/Sicilian Synergy/PlayerMulti.py
+++ codebase/Sicilian Synergy/PlayerMulti.py
@@ -7,6 +7,7 @@ import torch.optim as optim
 import sklearn
 from sklearn import metrics as metrics
 import pandas as pd
+import math
 
 def data_loader(path, table_idx, player_or_dealer):
     data = pd.read_csv(path, header=[0,1,2])
@@ -21,21 +22,117 @@ def sicily_data_loader(path, player_or_dealer):
     return np.array([spy, card]).T
 
 class MyPlayerMulti:
-    def __init__(self,lag=int(5)):
-        pass
-  
+    def __init__(self, lag=5):
+        self.lag = lag
+        self.player_models = {}
+        self.dealer_models = {}
+        self.game_history = {}
+        self.table_correlations = {}
+        self.prediction_history = {}
+        
+    def get_card_value_from_spy_value(self, value):
+        """Convert spy value to card value using the given formula"""
+        value += 100
+        value += 2.5
+        value = math.trunc(value)
+        value = value % 10
+        if value <= 1:
+            value += 10
+        return value
+    
     def choose_tables(self):
         """
-        return the indices of the tables you would like to play at. List of streams received in get_player_action_multi will be of the tables you provide below
-        followed by the sicily table in the end
-
-        The body is random for now, change accordingly
-        Make sure that each of the index returned by this function can be loaded using the data_loader function above
-
-        Return-> list[int] of the tables to play on
+        Return the indices of the tables to play on.
+        The Sicilian table will be appended to this list automatically.
         
+        Based on preliminary analysis, tables 0 and 1 seem to have useful patterns
+        that might correlate with the Sicilian table.
         """
-        return [0,1]
+        return [0, 1]
+    
+    def _analyze_correlations(self, list_spy_history_player, list_spy_history_dealer):
+        """Analyze correlations between table spy values to detect patterns"""
+        if any(len(hist) < self.lag for hist in list_spy_history_player):
+            return
+        sicily_idx = len(list_spy_history_player) - 1
+        for table_idx in range(sicily_idx):
+            for shift in range(1, min(len(list_spy_history_player[table_idx]), 10)):
+                if len(list_spy_history_player[table_idx]) >= shift + self.lag and len(list_spy_history_player[sicily_idx]) >= self.lag:
+                    shifted_player = list_spy_history_player[table_idx][-(shift+self.lag):-shift]
+                    sicily_player = list_spy_history_player[sicily_idx][-self.lag:]
+                    try:
+                        correlation = np.corrcoef(shifted_player, sicily_player)[0, 1]
+                        if abs(correlation) > 0.7:
+                            key = f"player_{table_idx}_sicily"
+                            self.table_correlations[key] = {
+                                'shift': shift,
+                                'correlation': correlation
+                            }
+                    except:
+                        pass
+                if len(list_spy_history_dealer[table_idx]) >= shift + self.lag and len(list_spy_history_dealer[sicily_idx]) >= self.lag:
+                    shifted_dealer = list_spy_history_dealer[table_idx][-(shift+self.lag):-shift]
+                    sicily_dealer = list_spy_history_dealer[sicily_idx][-self.lag:]
+                    try:
+                        correlation = np.corrcoef(shifted_dealer, sicily_dealer)[0, 1]
+                        if abs(correlation) > 0.7:
+                            key = f"dealer_{table_idx}_sicily"
+                            self.table_correlations[key] = {
+                                'shift': shift,
+                                'correlation': correlation
+                            }
+                    except:
+                        pass
+    
+    def _predict_next_card(self, spy_history, player_or_dealer, table_idx):
+        """Predict the next card based on spy history"""
+        if len(spy_history) < self.lag:
+            return self.get_card_value_from_spy_value(np.mean(spy_history))
+        sicily_idx = len(self.choose_tables())
+        if table_idx == sicily_idx:
+            for corr_table_idx in range(sicily_idx):
+                key = f"{player_or_dealer}_{corr_table_idx}_sicily"
+                if key in self.table_correlations:
+                    corr_info = self.table_correlations[key]
+                    shift = corr_info['shift']
+                    if player_or_dealer == 'player':
+                        correlated_spy_histories = self.current_spy_histories_player
+                    else:
+                        correlated_spy_histories = self.current_spy_histories_dealer
+                    if corr_table_idx < len(correlated_spy_histories) and len(correlated_spy_histories[corr_table_idx]) > shift:
+                        correlated_spy = correlated_spy_histories[corr_table_idx][-shift]
+                        return self.get_card_value_from_spy_value(correlated_spy)
+        recent_spy = spy_history[-self.lag:]
+        if len(recent_spy) > 1:
+            slope = (recent_spy[-1] - recent_spy[0]) / (len(recent_spy) - 1)
+            next_spy = recent_spy[-1] + slope
+        else:
+            next_spy = recent_spy[-1]
+        return self.get_card_value_from_spy_value(next_spy)
+    
+    def _make_decision(self, player_total, dealer_total, predicted_next_card, turn, table_idx):
+        """Make a strategic decision based on current totals and predicted next card"""
+        if turn == 'player':
+            next_total = player_total + predicted_next_card
+            if next_total <= 21:
+                return "hit"
+            if table_idx == len(self.choose_tables()):
+                has_strong_correlation = any(
+                    "sicily" in key and abs(info['correlation']) > 0.8 
+                    for key, info in self.table_correlations.items()
+                )
+                if has_strong_correlation and player_total < 17 and predicted_next_card <= 10:
+                    return "hit"
+            return "stand"
+        
+        else:  
+            predicted_dealer_total = dealer_total + predicted_next_card
+            if predicted_dealer_total > 21:
+                return "continue"
+            if predicted_dealer_total > player_total:
+                return "surrender"
+            return "continue"
+    
     def get_player_action_multi(self,
                             list_curr_spy_history_player, 
                             list_curr_spy_history_dealer,
@@ -48,41 +145,28 @@ class MyPlayerMulti:
                             game_index,
                             ):
         """
-        Arguments:
-        list_curr_spy_history_player: list[list] -> real number spy value series of player observed upto this point
-        list_curr_spy_history_dealer: list[list] -> real number spy value series of dealer observed upto this point
-        list_curr_card_history_player: list[list] -> integer series of player denoting value of cards observed upto this point
-        list_curr_card_history_dealer: list[list] -> integer series of dealer denoting value of cards observed upto this point
-        list_curr_player_total: list[int] -> list of integer score of player
-        list_curr_dealer_total: list[int] -> list integer score of dealer
-        turn: string -> either "player" or "dealer" denoting if its the player drawing right now or the dealer opening her cards
-        active_tables: list[bool] -> tells on which tables we can make an action . If turn="player" then it is True for tables we have not busted at or not stood at
-        If turn="dealer", then it is True on tables where you have not surrendered, and dealer is below 16.
-        game_index: integer -> tells which game is going on. Can be useful to figure if a new game has started
-
-        Note that correspopding lists of corresponding card and spy values are of the same length.  For eg the series  of table 1 player card and spy valus are of the same length.
-        Each of the above series have the same length = number of tables you choose to play at + 1 (for the sicily table)
+        Make decisions for all tables simultaneously.
         
-        The ordering is also same which you provided via the choose_tables function
-
-        The body is random for now, change accordingly
-
-        Output:
-            if turn=="player" output a list of size len(active_tables) (this length is same as the length of all other list arguments as well). Each argument should be "stand" or "hit". The action
-            for tables which have their entry for active_tables as False will be no-op.
-            else if turn=="dealer" output a list of size len(active_tables) (this length is same as the length of all other list arguments as well). Each argument should be "continue" or "surrender". 
-            The action for tables which have their entry for active_tables as False will be no-op.
+        Returns a list of actions for each table:
+        - For player's turn: "hit" or "stand"
+        - For dealer's turn: "continue" or "surrender"
         """
-        print(list_curr_spy_history_player, list_curr_spy_history_dealer,
-                            list_curr_card_history_player, 
-                            list_curr_card_history_dealer, 
-                            list_curr_player_total, 
-                            list_curr_dealer_total, 
-                            turn, 
-                            active_tables,
-                            game_index,)
-        if turn=='player':
-            return ['stand','stand','stand']
-
-        elif turn=='dealer':
-            return ['surrender','surrender','surrender']
+        self.current_spy_histories_player = list_curr_spy_history_player
+        self.current_spy_histories_dealer = list_curr_spy_history_dealer
+        self._analyze_correlations(list_curr_spy_history_player, list_curr_spy_history_dealer)
+        actions = []
+        for i, active in enumerate(active_tables):
+            if not active:
+                actions.append("stand" if turn == "player" else "continue")
+                continue
+            player_spy_history = list_curr_spy_history_player[i]
+            dealer_spy_history = list_curr_spy_history_dealer[i]
+            player_total = list_curr_player_total[i]
+            dealer_total = list_curr_dealer_total[i]
+            if turn == "player":
+                predicted_card = self._predict_next_card(player_spy_history, "player", i)
+            else:
+                predicted_card = self._predict_next_card(dealer_spy_history, "dealer", i)
+            action = self._make_decision(player_total, dealer_total, predicted_card, turn, i)
+            actions.append(action)
+        return actions
